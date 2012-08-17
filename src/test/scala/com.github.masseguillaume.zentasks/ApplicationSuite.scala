fae072c5fe315f7c527bd1d1adcb22ecc31d7133
Guillaume Massé <masgui@gmail.com>
2012-08-17 14:08:42 -0400

    ammend

--- src/test/scala/com.github.masseguillaume.zentasks/ApplicationSuite.scala
+++ src/test/scala/com.github.masseguillaume.zentasks/ApplicationSuite.scala
@@ -1,12 +1,47 @@
 package com.github.masseguillaume.zentasks
 
-/**
- * Created with IntelliJ IDEA.
- * User: guillaume
- * Date: 12-08-16
- * Time: 3:39 PM
- * To change this template use File | Settings | File Templates.
- */
-trait ApplicationSuite {
+import org.scalatest.{FunSpec, BeforeAndAfterAll}
+import org.scalatest.selenium.Firefox
+import org.scalatest.selenium.WebBrowser.{click, go, Query}
+import org.openqa.selenium.WebElement
 
+trait LoggedApplicationSuite
+  extends ApplicationSuite { self: FunSpec =>
+  override def beforeAll() {
+    corectLogin()
+  }
+}
+
+trait ApplicationSuite extends BeforeAndAfterAll with Firefox { this: FunSpec =>
+
+  val playHost = "http://localhost:9000"
+
+  implicit def workAround(query: Query): WebElement = query.getWebElement // scalatest bug
+
+  protected def corectLogin() { login( "guillaume@sample.com", "secret" ) }
+  protected def incorectLogin() { login( "1", "1" ) }
+
+  private def login( email: String, password: String )
+  {
+    go to ( playHost )
+
+    click on id( "email" )
+    textField( "email" ).value = email
+
+    click on id( "password" )
+    textField( "password" ).value = password
+
+    submit
+  }
+
+  def classWithText( clazz: String, text: String ): Boolean =
+  {
+    findAll( className( clazz ) ).exists( element => {
+      element.underlying.getText == text
+    })
+  }
+
+  override def afterAll(){
+  //  close()
+  }
 }
